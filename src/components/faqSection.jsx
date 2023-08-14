94154c5c6dee25b6910d9e4c68c067a3c63d6da5
Mauritz Marais <mauritz303@gmail.com>
2023-08-14 13:53:48 +0200

    added expand functionality to view answers

--- src/components/faqSection.jsx
+++ src/components/faqSection.jsx
@@ -1,1 +1,29 @@
 // faq section, question - answer
+import { useState } from "react";
+
+function Faq({ faq }) {
+
+    const [active, setActive] = useState('');
+
+    return (
+        <>
+            <h3>Frequently asked questions</h3>
+            {faq?.map(item => {
+                return <div className={`faq-question-container ${item.id == active && "active"}`} key={item.id} onClick={() => setActive(item.id)} >
+                    <p>{item.question}</p>
+
+
+                    <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M480-345 240-585l43-43 197 198 197-197 43 43-240 239Z" /></svg>
+
+
+                    <p className="answer" >{item.answer}</p>
+
+                </div>
+            })}
+        </>
+    )
+}
+
+export default Faq;
+
+// click on div toggle hidden class on last p element, rotate svg .5turn
