84986b763169bca275c6a9fbc82396789538cdc3
Mauritz Marais <mauritz303@gmail.com>
2023-08-14 13:56:48 +0200

    added images and content

--- src/components/projectsSection.jsx
+++ src/components/projectsSection.jsx
@@ -7,20 +7,18 @@ function Projects() {
             <section>
                 <h3>Previous Projects</h3>
                 <div className="projects-container">
-                    {projects?.map(item => {
-                        return <div className="project-card" key={item.id}>
-                            <picture>
-                                <source media="(max-width: 799px)" srcSet={item.image} />
-                                <source media="(min-width: 800px)" srcSet={item.image} />
-                                <img src={item.image} alt={item.imgAlt} />
-                            </picture>
+                    {projects?.map(item => <div className="project-card" key={item.id}>
+                        <h4>{item.title}</h4>
 
-                            <h4>{item.title}</h4>
+                        <picture>
+                            <source media="(max-width: 799px)" srcSet={item.image} />
+                            <source media="(min-width: 800px)" srcSet={item.image} />
+                            <div><img src={item.image} alt={item.imgAlt} /></div>
+                        </picture>
 
-                            <p>{item.testimonial}</p>
-                            <p>{item.clientName}</p>
-                        </div>
-                    })}
+                        <p>{item.testimonial}</p>
+                        <p>{item.clientName}</p>
+                    </div>)}
                 </div>
             </section>
         </>
