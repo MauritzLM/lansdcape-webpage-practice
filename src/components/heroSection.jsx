f71f2d58758a9a1265989a49ed688a9c0e93d251
Mauritz Marais <mauritz303@gmail.com>
2023-08-14 13:55:12 +0200

    added class to info section

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -3,14 +3,14 @@ function Hero() {
     return (
         <>
             <header>
-                <div>
+                <div className="hero">
                     <h1>The Landscaping Company</h1>
                     <p>Your Garden is our Passion</p>
                     <button>Learn more</button>
                 </div>
 
-                <div>
-                    <p>We are a landscaping company based in Pretoria, who has been designing and maintaining
+                <div className="hero-info">
+                    <p>We are a landscaping company based in Metro City, who has been designing and maintaining
                         gardens for over 20 years.</p>
 
                     <p>We have a team of passionate professionals ready to get their hands dirty</p>
