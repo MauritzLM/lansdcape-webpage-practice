5b5b186153e360b72a737ba31e25e4aaf6e22d1e
Mauritz Marais <mauritz303@gmail.com>
2023-08-16 17:16:03 +0200

    added div wrapper

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -4,24 +4,26 @@ function ContactUs() {
     return (
         <>
             <section className="contact-section">
-                <div>
-                    <h3>Contact Us</h3>
-                    <p>Leave us a message and we will get back to you within the hour.</p>
-                </div>
+                <div className="contact-section-container">
+                    <div className="contact-section-info">
+                        <h3>Contact Us</h3>
+                        <p>Leave us a message and we will get back to you within the hour.</p>
+                    </div>
 
-                <div>
-                    <form>
-                        <label htmlFor="name">Name</label>
-                        <input name="name" type="text" ></input>
+                    <div>
+                        <form>
+                            <label htmlFor="name">Name</label>
+                            <input name="name" type="text" ></input>
 
-                        <label htmlFor="email">email</label>
-                        <input name="email" type="email"></input>
+                            <label htmlFor="email">email</label>
+                            <input name="email" type="email"></input>
 
-                        <label htmlFor="message">Your message</label>
-                        <textarea name="message" id="" cols="30" rows="5"></textarea>
+                            <label htmlFor="message">Your message</label>
+                            <textarea name="message" id="" cols="30" rows="5"></textarea>
 
-                        <button>Submit</button>
-                    </form>
+                            <button>Submit</button>
+                        </form>
+                    </div>
                 </div>
             </section>
         </>
